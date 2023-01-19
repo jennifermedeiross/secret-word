f277bdd7140ebc8961314909bafa09eb5087927d
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-01-19 15:12:46 -0300

    Evento de inserção de letra

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import './Game.css';
 
 export const Game = ({ 
@@ -10,6 +11,18 @@ export const Game = ({
   guesses,
   score
 }) => {
+  const [letter, setLetter] = useState("");
+  const letterInputRef = useRef(null)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    verifyLetter(letter);
+    setLetter("");
+
+    letterInputRef.current.focus();
+  };
+
   return (
     <div className='game'>
       <p className='points'>
@@ -33,16 +46,24 @@ export const Game = ({
       </div>
       <div className='letterContainer'>
         <p>Tente adivinhar uma letra da palavra:</p>
-        <form>
-        </form>
-          <input type="text" name="letter" maxLength="1" required />
+        <form onSubmit={handleSubmit}>
+          <input 
+          type="text" 
+          name="letter" 
+          maxLength="1" 
+          required
+          onChange={(e) => setLetter(e.target.value)}
+          value={letter}
+          ref={letterInputRef} 
+          />
           <button>Jogar</button>
+        </form>
       </div>
 
       <div className="wrongLetterContainer">
         <p>Letras já utilizadas:</p>
         {wrongLetters.map((wrongLetter, i) => (
-          <span key={i}> {wrongLetter} |</span>
+          <span key={i}>{wrongLetter} | </span>
         ))}
       </div>
     </div>
