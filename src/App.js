bc1f67fcedb7226f0d1d82abe8117246b86dd407
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-01-19 12:00:39 -0300

    Tela inicial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StartScreen from './components/StartScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <StartScreen />
+    </div>
+  );
+}
+
+export default App;
