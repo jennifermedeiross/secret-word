9a9de18db11b9e877ec5c85c2c2061cabac4c809
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-01-19 13:23:33 -0300

    Escolhendo palavra e categoria aleatoriamente

--- src/App.js
+++ src/App.js
@@ -22,7 +22,28 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return {word, category};
+  }
+
   const startGame = () => {
+    const {word, category} = pickWordAndCategory();
+    
+    let wordLetters = word.toLowerCase().split('');
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+    
     setGameStage(stages[1].name)
   }
 
--- src/components/GameOver/index.js
+++ src/components/GameOver/index.js
@@ -4,7 +4,7 @@ export const GameOver = ( { retry }) => {
   return (
     <div>
       <h1>GameOver</h1>
-      <button onClick={retry}>Recomçar</button>
+      <button onClick={retry}>Recomeçar</button>
     </div>
   )
 }
